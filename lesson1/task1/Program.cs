92068fd4d9a58dc1f9eab469d102fcd3d2e28701
Roman Krotov <krotoroman@gmail.com>
2023-01-13 23:50:41 +0300

    done task1

---
+++ lesson1/task1/Program.cs
@@ -0,0 +1,10 @@
+﻿int numberA = new Random().Next (1, 10); 
+Console.WriteLine($"Первое число: {numberA}");
+int numberB = new Random().Next (1, 10);
+Console.WriteLine($"Второе число: {numberB}");
+if (numberA > numberB) {
+	Console.WriteLine($"Результат: {numberA} больше, чем число {numberB}");
+}
+else {
+	Console.WriteLine($"Результат: {numberB} больше, чем число {numberA}");
+}
