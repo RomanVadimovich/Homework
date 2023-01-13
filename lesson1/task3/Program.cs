de8cfa7cc04af7423ab08035ad9ea46684fcac76
Roman Krotov <krotoroman@gmail.com>
2023-01-14 00:12:16 +0300

    done task3

---
+++ lesson1/task3/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Введите любое число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+if (n % 2 == 0)
+{
+	Console.WriteLine($"Это число является четным: {n}");
+} 
+else
+{
+	Console.WriteLine($"Число {n} является нечетным!/ Загадайте другое.");
+}
